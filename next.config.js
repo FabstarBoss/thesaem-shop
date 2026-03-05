949493c4b14e1bde7f24cf0ca96ab548b7a50981
Dmitriy Gudovskiy <speccops@MacBook-Pro-Dmitriy.local>
2026-03-05 09:13:26 +0800

    add next config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ['static.insales-cdn.com'],
+  },
+}
+module.exports = nextConfig
